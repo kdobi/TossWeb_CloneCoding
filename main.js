13046c7032875a67bf6c56faa6fcd1c503d2fba0
김동빈 <kdobi87@naver.com>
2025-10-30 20:30:41 +0900

    feat: section1 페이드인 효과

---
+++ main.js
@@ -0,0 +1,16 @@
+
+
+let home1_content = document.querySelector(".home1_content");
+let op_count = 0;
+
+
+setTimeout(()=> {
+const fadein = setInterval(() => {
+  op_count = op_count + 0.1; 
+  home1_content.style.opacity = op_count;
+
+  if (op_count >= 1) {
+    clearInterval(fadein); 
+  }
+}, 50); 
+}, 1000);
