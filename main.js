67e05476f55b84e2db07c85e09110c0d53836e50
김동빈 <kdobi87@naver.com>
2026-01-10 15:04:21 +0900

    feat : 스크롤 페이드인 기능 구현

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 //  section1 홈화면 인트로 
 let home1_content = document.querySelector(".home1_content");
 let op_count = 0;
+setTimeout(() => {
+  const fadein = setInterval(() => {
+    op_count = op_count + 0.1;
+    home1_content.style.opacity = op_count;
 
-
-setTimeout(()=> {
-const fadein = setInterval(() => {
-  op_count = op_count + 0.1; 
-  home1_content.style.opacity = op_count;
-
-  if (op_count >= 1) {
-    clearInterval(fadein); 
-  }
-}, 50); 
+    if (op_count >= 1) {
+      clearInterval(fadein);
+    }
+  }, 50);
 }, 1000);
 
+// 페이지 진입시 페이드인 효과 
+const options = {
+  threshold: 0.15,
+  rootMargin: "0px 0px -10% 0px",
+}
+
+const targets = document.querySelectorAll('.reveal')
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("is-visible");
+    }
+  });
+
+}, options);
+
+targets.forEach((el) => observer.observe(el));
 
